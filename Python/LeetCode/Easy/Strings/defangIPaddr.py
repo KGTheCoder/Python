4d09de200f736aedffa9c598d86e2c6397c0ee82
KG <kgitts98@gmail.com>
2022-07-16 09:04:07 -0700

    .

--- Python/LeetCode/Easy/Strings/defangIPaddr.py
+++ Python/LeetCode/Easy/Strings/defangIPaddr.py
@@ -5,6 +5,16 @@ class Solution:
     def defangIPaddr_2(self, address: str) -> str:
         return '[.]'.join(address.split('.'))
 
+    def defangIPaddr_3(self, address: str) -> str:
+        ans = []
+        for ch in address:
+            if ch.isdigit():
+                ans.append(ch)
+            else:
+                ans.append("[.]")
+        return "".join(ans)
+
 sol = Solution()
 # print(sol.defangIPaddr("1.1.1.1"))
-print(sol.defangIPaddr_2("1.1.1.1"))
+# print(sol.defangIPaddr_2("1.1.1.1"))
+# print(sol.defangIPaddr_3("1.1.1.1"))
