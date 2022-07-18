13b03477dc62ee902e8018de8d2ba4f63bfa2102
KG <kgitts98@gmail.com>
2022-07-18 11:06:48 -0700

    .

--- Python/LeetCode/Easy/Strings/removeVowel.py
+++ Python/LeetCode/Easy/Strings/removeVowel.py
@@ -1,3 +1,27 @@
 class Solution: 
     def removeVowels(self, s: str) -> str:
-        
+        ans = ""
+        for letter in s:
+            if letter not in "aeiou":
+                ans += letter
+        return ans
+
+    def removeVowels_2(self, s: str) -> str:
+        ans = []
+        for letter in s:
+            if letter not in "aeiou":
+                ans.append(letter)
+        return "".join(ans)
+
+    def removeVowels_3(self, s: str) -> str:
+        ans = []
+        vowels = set("aeiou")
+        for letter in s:
+            if letter not in vowels:
+                ans.append(letter)
+        return "".join(ans)
+
+sol = Solution()
+# print(sol.removeVowels("This is a great message!"))
+# print(sol.removeVowels_2("This is a great message!"))
+print(sol.removeVowels_3("This is a great message!"))
