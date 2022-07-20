11aba92220709ead7b72d106f9cb7cfadf7aa1f0
KG <kgitts98@gmail.com>
2022-07-20 12:35:20 -0700

    .

--- Python/LeetCode/Easy/Hash Table/numJewelsInStones.py
+++ Python/LeetCode/Easy/Hash Table/numJewelsInStones.py
@@ -8,8 +8,26 @@ class Solution:
 
     def numJewelsInStones_3(self, jewels: str, stones: str) -> int:
         return sum(map(jewels.count, stones))
+    
+    def numJewelsInStones_4(self, j: str, s: str) -> int:
+        return sum(map(s.count, j))
+    
+    def numJewelsInStones_5(self, J, S):
+        charToFreqS = {}
+        numJewels = 0
+
+        for ch in S:
+            if ch not in charToFreqS:
+                charToFreqS[ch] = 1
+            else:
+                charToFreqS[ch] += 1
+        
+        for ch in J:
+            if ch in charToFreqS:
+                numJewels += charToFreqS[ch]
+        
+        return numJewels
 
 sol = Solution()
-# print(sol.numJewelsInStones("aAabbA", "BbaAabAAb"))
-# print(sol.numJewelsInStones_2("aAabbA", "BbaAabAAb"))
-print(sol.numJewelsInStones_3("aAabbA", "BbaAabAAb"))
+print(sol.numJewelsInStones("aA", "aAAbbbb"))
+print(sol.numJewelsInStones_5("aA", "aAAbbbb"))
--- Python/_misc/misc_8.py
+++ Python/_misc/misc_8.py
@@ -1,2 +1,69 @@
-operations = ["X++", "++X", "--X", "X--"]
-print(len(operations))
+"""
+def addition(n):
+    return n + n
+
+nums = (1, 2, 3, 4)
+result = map(addition, nums)
+print(list(result))
+"""
+
+"""
+nums = [1, 2, 3, 4, 5]
+squared = []
+
+for num in nums:
+    squared.append(num **2)
+
+print(squared)
+
+# --------------------
+
+def square(num):
+    return num **2
+
+nums = [1, 2, 3, 4, 5]
+squared = map(square, nums)
+
+print(list(squared))
+"""
+
+"""
+str_nums = ["4", "8", "6", "5", "3", "2", "8", "9", "2", "5"]
+
+int_nums = map(int, str_nums)
+
+print(list(int_nums))
+"""
+
+"""
+nums = [-2, -1, 0, 1, 2]
+
+abs_values = list(map(abs, nums))
+print(abs_values)
+
+print(list(map(float, nums)))
+
+words = ["Welcome", "to", "My", "World"]
+print(list(map(len, words)))
+"""
+
+"""
+nums = [1, 2, 3, 4, 5]
+squared = map(lambda num: num **  2, nums)
+
+print(list(squared))
+"""
+
+"""
+first_it = [1, 2, 3]
+second_it = [4, 5, 6, 7]
+
+print(list(map(pow, first_it, second_it)))
+"""
+
+"""
+print(list(map(lambda x, y: x - y, [2, 4, 6], [1, 3, 5])))
+print(list(map(lambda x, y, z: x + y + z, [2, 4], [1, 3], [7,8])))
+"""
+
+
