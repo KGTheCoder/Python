c75e7da0e387b1333309b291eab3b2fa8c55d0cf
KG <kgitts98@gmail.com>
2022-07-26 08:15:34 -0700

    .

--- Python/LeetCode/Easy/Array/movingAverage.py
+++ Python/LeetCode/Easy/Array/movingAverage.py
@@ -1,4 +1,5 @@
 class MovingAverage:
+    """
     def __init__(self, size: int):
         self.size = size
         self.queue = []
@@ -9,8 +10,19 @@ class MovingAverage:
         window_sum = sum(queue[-size:])
 
         return window_sum / min(len(queue), size)
+    """
+    def __init__(self, size: int):
+        self.array = []
+        self.size = size
+    
+    def next(self, val: int) -> float:
+        self.array.append(val)
+        return sum(self.array[-self.size:]) / min(len(self.array), self.size)
 
 MA = MovingAverage(3)
-print(MA.next(4))
+print(MA.next(1))
+print(MA.next(10))
+print(MA.next(3))
+print(MA.next(5))
 
 
--- Python/_misc/misc_14.py
+++ Python/_misc/misc_14.py
@@ -1,4 +1,22 @@
+"""
 x = 3.0
 y = 4.0
 
 print((x + y) / 2)
+"""
+
+array = []
+size = 3
+val = 1
+val_2 = 10
+val_3 = 3
+val_4 = 5
+
+array.append(val)
+array.append(val_2)
+array.append(val_3)
+array.append(val_4)
+
+print(array)
+
+print(sum(array[-size]))
