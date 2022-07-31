97a40b31c0cd4f9110bd41ae0677eeb9fc4c947b
KG <kgitts98@gmail.com>
2022-07-31 08:05:46 -0700

    ...

--- Python/LeetCode/Easy/Array/running_sum.py
+++ Python/LeetCode/Easy/Array/running_sum.py
@@ -4,7 +4,17 @@ class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         for i in range(1, len(nums)):
             nums[i] += nums[i - 1]
+        return nums
+    
+    def runningSum_2(self, nums: List[int]) -> List[int]:
+        i = 1
+        while i < len(nums):
+            nums[i]+=nums[i-1]
+            i+=1
+        return nums 
+
 
 
 sol = Solution()
-print(sol.runningSum([1,2,3,4]))
+# print(sol.runningSum([1,2,3,4]))
+print(sol.runningSum_2([1,2,3,4]))
